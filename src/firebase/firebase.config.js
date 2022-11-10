12329ea5a089f3878109ce01fc674928fd5a8e57
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-11-10 23:24:33 +0600

    final commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,18 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId
-  apiKey: "AIzaSyDCqdIVQDWpLeA70nQghW2f4Ee8OhlSiJs",
-  authDomain: "detective-kevin.firebaseapp.com",
-  projectId: "detective-kevin",
-  storageBucket: "detective-kevin.appspot.com",
-  messagingSenderId: "74021519753",
-  appId: "1:74021519753:web:712f3f47ed284f88d62158"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
+  
 };
 
 // Initialize Firebase
